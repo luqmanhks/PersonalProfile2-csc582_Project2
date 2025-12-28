b6259afbcb47c96013e820ea3c048d1a68a23519
MUHAMMAD LUQMAN HAKIM BIN KHAIRUL SALLEH <luqmanhks21@gmail.com>
2025-12-28 22:26:26 +0800

    Delete Luqman_PersonalProfileApp2/databese.sql

--- Luqman_PersonalProfileApp2/databese.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE profiles (
-    id INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    name VARCHAR(100),
-    student_id VARCHAR(20) PRIMARY KEY,
-    program VARCHAR(100),
-    email VARCHAR(100),
-    hobbies VARCHAR(255),
-    self_intro CLOB
-);
